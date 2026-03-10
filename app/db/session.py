72acb1c65a3f64d833d19831c753c1d7345990a9
Kacper Chlastawa <kacper.chlastawa03@gmail.com>
2026-03-10 14:47:38 +0100

    session created

---
+++ app/db/session.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.core.config import settings
+from typing import AsyncGenerator
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo = True,
+    future = True
+)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False
+)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
+
+
+
+
