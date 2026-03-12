56e933bace0d0ebe14a3ea174c22af4f2313e647
Kacper Chlastawa <kacper.chlastawa03@gmail.com>
2026-03-12 10:49:40 +0100

    Add alembic configuration and initial migration/ Docker bugs fixed

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,13 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-
-    model_config = SettingsConfigDict(env_file=".env")
-
+    aws_access_key_id: str
+    aws_secret_access_key: str
+    aws_region: str
+    s3_bucket_name: str
+    aws_endpoint_url: str
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"  # To sprawi, że nadmiarowe zmienne w .env nie będą powodować błędów
+    )
 settings = Settings()
