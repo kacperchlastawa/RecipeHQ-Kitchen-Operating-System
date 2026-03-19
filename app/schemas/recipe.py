29f145880f2a699705d7327c8f385675ec453b44
Kacper Chlastawa <kacper.chlastawa03@gmail.com>
2026-03-19 16:39:54 +0100

    Fixing some warnings

--- app/schemas/recipe.py
+++ app/schemas/recipe.py
@@ -1,33 +1,29 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional, List
 
 class RecipeBase(BaseModel):
-    title: str = Field(..., min_length=3, max_length=255, example="Spaghetti Carbonara")
-    description: Optional[str] = Field(None, example="Tradycyjny włoski przepis na baziej jajek i guanciale.")
-    cooking_time: int = Field(..., gt=0, description="Czas w minutach", example=25)
-    difficulty: str = Field(..., example="Medium")  # Możemy tu później dodać Enum
-    kcal: Optional[int] = Field(None, gt=0, example=650)
+    title: str = Field(..., min_length=3, max_length=255,json_schema_extra={"example":"Spaghetti Carbonara"})
+    description: Optional[str] = Field(None, json_schema_extra={"example":"Tradycyjny włoski przepis na bazie jajek i guanciale."})
+    cooking_time: int = Field(..., gt=0, json_schema_extra={"example":25})
+    difficulty: str = Field(..., json_schema_extra={"example":"Medium"})  # Dodać enum później
+    kcal: Optional[int] = Field(None, gt=0,json_schema_extra= {"example":650})
 
-    # Składniki i alergeny przesyłamy jako listy stringów
-    ingredients: List[str] = Field(..., example=["makaron spaghetti", "jajka", "guanciale", "pecorino"])
-    allergens: Optional[List[str]] = Field(default_factory=list, example=["gluten", "jaja", "laktoza"])
+    # Składniki i alergeny jako listy stringów
+    ingredients: List[str] = Field(...,json_schema_extra={"example":["makaron spaghetti", "jajka", "guanciale", "pecorino"]})
+    allergens: Optional[List[str]] = Field(default_factory=list, json_schema_extra={"example":["gluten", "jaja", "laktoza"]})
 
-# Schemat używany przy tworzeniu przepisu (POST)
 class RecipeCreate(RecipeBase):
     pass
 
-# Schemat używany przy aktualizacji (PATCH) - wszystkie pola opcjonalne
 class RecipeUpdate(RecipeBase):
     title: Optional[str] = None
     cooking_time: Optional[int] = None
     ingredients: Optional[List[str]] = None
     difficulty: Optional[str] = None
 
-# Schemat używany do zwracania danych z API (Response)
 class RecipeResponse(RecipeBase):
     id: int
     owner_id: int
     image_url: Optional[str] = None
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
